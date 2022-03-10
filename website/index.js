68d5c15d5b5142cfa19d6479e778a4cf7b36e1ae
Luis <hypnoticsiege@gmail.com>
2022-03-10 08:11:48 -0500

    :fax: Downloadable Files, Nav, Labels on home
    After merging the "viewfiles" page, it was a pretty bad version in BETA. I made the files a list going through each file. Also includes downloadable content straight from the page.
    Also a new navbar to look cleaner and cooler imo.
    Some little labels on the home page.. and thats really it!
    :)

--- website/index.js
+++ website/index.js
@@ -12,13 +12,14 @@ app.use(multer({
     storage: multerStorage
 }).any());
 app.use(express.static('public'));
+app.use("/files", express.static("files"));
 app.set('view engine', 'ejs');
 
-app.get('/', async function(req, res) {
+app.get('/', async function (req, res) {
     res.render('index', {});
 });
 
-app.post('/sendform', async function(req, res) {
+app.post('/sendform', async function (req, res) {
     const content = req.body.content;
     const time = Date.now();
     const fileName = req.body.name;
@@ -30,19 +31,21 @@ app.post('/sendform', async function(req, res) {
     res.download(`./files/${fileName}${config.siteInformation.fileExtension.replaceAll(".", "")}`);
     fs.writeFileSync(`./backups/${fileName}-${time}.js`, content);
     setTimeout(() => {
-        fs.unlink(`./files/${fileName}${config.siteInformation.extension}`, function() {});
+        fs.unlink(`./files/${fileName}${config.siteInformation.extension}`, function () {});
     }, 60000);
 });
 
-app.get("/viewfiles", async function(req, res) {
+app.get("/viewfiles", async function (req, res) {
     const files = fs.readdirSync("./files").filter(file => file.endsWith(config.siteInformation.fileExtension.replaceAll(".", "")));
-    res.render('viewfiles', {files});
+    res.render('viewfiles', {
+        files
+    });
 });
 
-app.listen(config.siteInformation.processPort, async function() {
+app.listen(config.siteInformation.processPort, async function () {
     console.log(`EasyCompile Started - Created by FAXES & Pluto.`)
 });
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     if (res.status(404)) res.redirect('/')
 });
