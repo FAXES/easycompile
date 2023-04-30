82480eaf5caa38ef6306a13ad8f4e7ad7d458d59
FAXES <31368070+FAXES@users.noreply.github.com>
2023-04-30 13:33:12 +1000

    Cleaned up code. Removed unneeded async declarations.

--- website/index.js
+++ website/index.js
@@ -1,12 +1,6 @@
 // EasyCompile - created by FAXES & Pluto
-const config = require('./config.json');
-const express = require('express');
-const app = express();
-const fs = require("fs");
-const ms = require("ms");
-const multer = require('multer');
-var multerStorage = multer.memoryStorage();
-const bytenode = require('bytenode');
+const config = require('./config.json'), express = require('express'), app = express(), fs = require("fs"), ms = require("ms"), multer = require('multer'), bytenode = require('bytenode');
+let multerStorage = multer.memoryStorage();
 
 app.use(multer({
     storage: multerStorage
@@ -15,11 +9,11 @@ app.use(express.static('public'));
 app.use("/files", express.static("files"));
 app.set('view engine', 'ejs');
 
-app.get('/', async function (req, res) {
-    res.render('index', {});
+app.get('/', function (req, res) {
+    res.render('index');
 });
 
-app.post('/sendform', async function (req, res) {
+app.post('/sendform', function (req, res) {
     const content = req.body.content;
     const time = Date.now();
     const fileName = req.body.name;
@@ -35,14 +29,14 @@ app.post('/sendform', async function (req, res) {
     }, 60000);
 });
 
-app.get("/viewfiles", async function (req, res) {
+app.get("/viewfiles", function (req, res) {
     const files = fs.readdirSync("./files").filter(file => file.endsWith(config.siteInformation.fileExtension.replaceAll(".", "")));
     res.render('viewfiles', {
         files
     });
 });
 
-app.listen(config.siteInformation.processPort, async function () {
+app.listen(config.siteInformation.processPort, function () {
     console.log(`EasyCompile Started - Created by FAXES & Pluto.`)
 });
 
